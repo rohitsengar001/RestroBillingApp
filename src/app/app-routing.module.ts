b35b965311598ec4dca90780cd95f4dd4922c756
rohitsengar001 <rohitsengar001@gmail.com>
2021-01-09 17:21:05 +0530

    pagenotfound routing successfully added at root routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BillingpdfComponent } from './modules/billing/billingpdf/billingpdf.component';
+import { PagenotfoundComponent } from './modules/layout/pagenotfound/pagenotfound.component';
 // import { HomeComponent } from './modules/billing/home/home.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/account/login',pathMatch:'full'},
-  {path:'account',loadChildren:()=>import ('./modules/account/account.module').then(m=>m.AccountModule)}
-  // {path: '', redirectTo: '/layout/bill/home', pathMatch: 'full'},
-  // {path: 'layout', loadChildren: () => import('./modules/layout/layout-routing.module').then(m => m.LayoutRoutingModule)},
-  // {path:"billingpdf",component:BillingpdfComponent}
+  {path:'account',loadChildren:()=>import ('./modules/account/account.module').then(m=>m.AccountModule)},
+  {path:'**',component:PagenotfoundComponent}
 ];
 
 @NgModule({
