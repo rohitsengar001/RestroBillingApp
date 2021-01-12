064c2372efc4a90bcbe01ebe9ada3dad0c258f04
rohitsengar001 <rohitsengar001@gmail.com>
2021-01-13 03:13:57 +0530

    toggle button successfully work in sidebar component

--- src/app/modules/layout/sidenavbar/sidenavbar.component.ts
+++ src/app/modules/layout/sidenavbar/sidenavbar.component.ts
@@ -5,10 +5,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenavbar.component.css']
 })
 export class SidenavbarComponent implements OnInit {
-
+  sidenavbarToggleClass="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
   constructor() { }
 
   ngOnInit(): void {
   }
+  onClickSidebarToggle(){
+    if(this.sidenavbarToggleClass=="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion")
+      this.sidenavbarToggleClass="navbar-nav bg-gradient-primary sidebar toggled sidebar-dark accordion"
+      else
+      this.sidenavbarToggleClass="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
 
+  }
+  
 }
